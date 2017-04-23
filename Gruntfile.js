5871f8900ebddfdcdade125616778e6da788562b
Omar Polo <omar.polo@europecom.net>
2017-04-23 10:56:19 +0200

    fixed bug from last merge + grunt from script in package

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 module.exports = function (grunt) {
-=======
-module.exports = function(grunt) {
->>>>>>> react
     grunt.initConfig({
         browserify: {
             dist: {
@@ -22,28 +18,6 @@ module.exports = function(grunt) {
                 }
             }
         },
-
-<<<<<<< HEAD
-        // sass: {
-        //     options: {
-        //         sourceMap: true
-        //     },
-        //     mobile: {
-        //         files: {
-        //             './public/assets/dist/styles/app_mobile.css': './public/assets/src/sass/app_mobile.scss'
-        //         }
-        //     },
-        //     tablet: {
-        //         files: {
-        //             './public/assets/dist/styles/app_tablet.css': './public/assets/src/sass/app_tablet.scss'
-        //         }
-        //     },
-        //     desktop: {
-        //         files: {
-        //             './public/assets/dist/styles/app.css': './public/assets/src/sass/app.scss'
-        //         }
-        //     }
-        // },
 
         stylus: {
             options: {
@@ -65,63 +39,6 @@ module.exports = function(grunt) {
             desktop: {
                 files: {
                     './public/assets/dist/styles/app.css': './public/assets/src/stylus/app.styl'
-                }
-            }
-        },
-
-        image: {
-            dynamic: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: './public/assets/src/img/',
-                        src: ['**/*.{png,jpg,jpeg,gif,svg}'],
-                        dest: './public/assets/dist/img/'
-                    }
-                ]
-            }
-        },
-
-        watch: {
-            scripts: {
-                files: ["./public/assets/src/script/**/*.js"],
-                tasks: ["browserify"]
-            },
-            styles: {
-                files: ["./public/assets/src/stylus/**/*.styl"],
-                tasks: ["stylus"]
-            },
-            // images: {
-            //   files: ["./public/assets/src/img/**/*.{png,jpg,jpeg,gif,svg}"]
-            // }
-        }
-    });
-
-    grunt.loadNpmTasks("grunt-browserify");
-    grunt.loadNpmTasks("grunt-contrib-stylus")
-    grunt.loadNpmTasks("grunt-contrib-watch");
-    grunt.loadNpmTasks("grunt-image");
-
-    grunt.registerTask("default", ["watch"]);
-    grunt.registerTask("build", ["browserify", "stylus", "image"]);
-=======
-        sass: {
-            options: {
-                sourceMap: true
-            },
-            mobile: {
-                files: {
-                    './public/assets/dist/styles/app_mobile.css': './public/assets/src/sass/app_mobile.scss'
-                }
-            },
-            tablet: {
-                files: {
-                    './public/assets/dist/styles/app_tablet.css': './public/assets/src/sass/app_tablet.scss'
-                }
-            },
-            desktop: {
-                files: {
-                    './public/assets/dist/styles/app.css': './public/assets/src/sass/app.scss'
                 }
             }
         },
@@ -168,8 +85,8 @@ module.exports = function(grunt) {
                 tasks: ["browserify"]
             },
             styles: {
-                files: ["./public/assets/src/sass/**/*.scss"],
-                tasks: ["sass"]
+                files: ["./public/assets/src/stylus/**/*.styl"],
+                tasks: ["stylus"]
             },
             // images: {
             //   files: ["./public/assets/src/img/**/*.{png,jpg,jpeg,gif,svg}"]
@@ -178,16 +95,15 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks("grunt-browserify");
-    grunt.loadNpmTasks("grunt-sass");
+    grunt.loadNpmTasks("grunt-contrib-stylus");
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-image");
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks('grunt-notify');
 
     grunt.registerTask("default", ["watch"]);
-    grunt.registerTask("build", ["browserify", "sass", "image"]);
+    grunt.registerTask("build", ["browserify", "stylus", "image"]);
     grunt.registerTask("optimize", ["browserify", "sass", "image", "uglify"]);
 
     grunt.task.run('notify_hooks');
->>>>>>> react
 };
