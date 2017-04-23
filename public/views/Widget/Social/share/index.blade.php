ce812e3a848286c5c4aae2d763cb36cec3427fdf
Mattia Carbone <mattiacarbone4@gmail.com>
2017-04-23 16:16:21 +0200

    icone

--- public/views/Widget/Social/share/index.blade.php
+++ public/views/Widget/Social/share/index.blade.php
@@ -2,39 +2,41 @@
   <h3>Condividi</h3>
   <ul>
     <li class="item-social">
-      <a href="#" title="condividi su Facebook">
-        <img src="#" />
+      <a href="http://www.facebook.com/sharer.php?u={{ the_permalink() }}" title="condividi su Facebook">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/facebook.svg" />
       </a>
     </li>
     <li class="item-social">
-      <a href="#" title="condividi su Twitter">
-        <img src="#" />
+      <a href="https://twitter.com/share?url={{ the_permalink() }}&amp;text=L'oppure,%20lo%20squardo%20sul%20territorio,%20ovvunque%20ti%20trovi&amp;hashtags=LoppureIT" title="condividi su Twitter">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/twitter.svg" />
+      </a>
+      <!-- TODO rivedere il text -->
+    </li>
+    <li class="item-social">
+      <a href="https://plus.google.com/share?url={{ the_permalink() }}" title="condividi su Google Plus">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/google-plus.svg" />
       </a>
     </li>
     <li class="item-social">
-      <a href="#" title="condividi su Google Plus">
-        <img src="#" />
+      <a href="http://www.linkedin.com/shareArticle?mini=true&amp;url={{ the_permalink() }}" title="condividi su LinkedIn">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/linkedin.svg" />
       </a>
     </li>
     <li class="item-social">
-      <a href="#" title="condividi su LinkedIn">
-        <img src="#" />
+      <a href="whatsapp://send?text={{ the_permalink() }}" data-action="share/whatsapp/share" title="condividi tramite Whasthapp">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/whatsapp.svg" />
       </a>
     </li>
     <li class="item-social">
-      <a href="#" title="condividi tramite Whasthapp">
-        <img src="#" />
+      <a href="https://t.me/share/url?url={{ the_permalink() }}" title="condividi tramite Telegram">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/telegram.svg" />
       </a>
     </li>
     <li class="item-social">
-      <a href="#" title="condividi tramite Telegram">
-        <img src="#" />
+      <a href="mailto:?Subject=Uno sguardo de L'oppure&amp;Body=L'oppure,%20lo%20squardo%20sul%20territorio,%20ovvunque%20ti%20trovi%20 {{ the_permalink() }}" title="condividi tramite mail">
+        <img src=" {{ get_template_directory_uri() }}/public/assets/src/img/icone-social/mail.svg" />
       </a>
-    </li>
-    <li class="item-social">
-      <a href="#" title="condividi tramite mail">
-        <img src="#" />
-      </a>
+      <!-- TODO text body mail -->
     </li>
   </ul>
 </div> <!-- .entry-social -->
