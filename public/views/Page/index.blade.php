cb4d6e002cfa574a06c01cae969e3687c070d6dc
Omar Polo <omar.polo@europecom.net>
2017-07-22 12:57:31 +0200

    fixed pages with no custom template

--- public/views/Page/index.blade.php
+++ public/views/Page/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 
 <div class="page-gray page-standard">
-	<header class="header-page">
-		<h1 class="title-page">{{ the_title() }}</h1>
-	</header>
-	<section class="content-page-text">
-		{{ $post->content }}
-	</section>
+    <header class="header-page">
+        <h1 class="title-page">{{ $title }}</h1>
+    </header>
+    <section class="content-page-text">
+        {{ $content }}
+    </section>
 </div>
 
 @endsection
