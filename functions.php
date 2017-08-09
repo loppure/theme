0442b6109c3a4c8fbb73aaac785396d1f1fd9e33
Omar Polo <omar.polo@europecom.net>
2017-08-09 16:37:06 +0200

    fixed syntax error

--- functions.php
+++ functions.php
@@ -56,7 +56,7 @@ $theme->run(); // lancia il tema ^^
 // TODO: add our custom post type
 add_filter('pre_get_posts', function($query) {
     if (is_home() && $query->is_main_query()) {
-        $query->set('post_type', ['post'])
+        $query->set('post_type', ['post']);
     }
 
     return $query;
