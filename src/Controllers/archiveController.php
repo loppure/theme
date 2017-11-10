181f188b3893fa99fd93326e5c53a36ffdb39b66
Omar Polo <omar.polo@europecom.net>
2017-11-10 11:40:46 +0100

    updated archive controller + deps

--- src/Controllers/archiveController.php
+++ src/Controllers/archiveController.php
@@ -8,7 +8,15 @@ class archiveController extends Controller
     {
         $url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-        if (strpos($url, '?citta=') === false && strpos($url, '?category=') === false) {
+        if (strpos($url, "polaroid")) {
+            parent::__construct('Archive.archive-post-type.archive-polaroid');
+        } else if (strpos($url, "eventi")) {
+            parent::__construct('Archive.archive-post-type.archive-eventi');
+        } else if (strpos($url, "reportage")) {
+            parent::__construct('Archive.archive-post-type.archive-reportage');
+        } else if (strpos($url, "team")) {
+            parent::__construct('Archive.archive-post-type.archive-team');
+        } else if (strpos($url, '?citta=') === false && strpos($url, '?category=') === false) {
             parent::__construct('Home.index');
         } else {
             parent::__construct('Archive.index');
